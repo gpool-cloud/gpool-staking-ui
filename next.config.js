bfd5442213d7028ae07802446c713f3b80210c92
Jamil Zakirov <djamilzak@gmail.com>
2025-02-06 14:37:13 -0300

    Update API URL

--- next.config.js
+++ next.config.js
@@ -4,7 +4,7 @@ const nextConfig = {
       return [
         {
           source: '/api/:path*',
-          destination: 'https://ore.gpool.cloud/:path*',
+          destination: 'https://api.gpool.cloud/:path*',
         },
       ];
     },
